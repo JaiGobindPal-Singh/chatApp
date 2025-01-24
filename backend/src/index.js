629e5f21d1c83e249707e12b570c28ba37796607
JaiGobindPalSingh <jai.gobind.pal@gmail.com>
2025-01-24 15:05:53 +0530

    connected the mongodb

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:'+PORT);
+  connectDB();
+});
