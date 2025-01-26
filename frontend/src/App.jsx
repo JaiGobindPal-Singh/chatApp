4853f5db78c5ac6d443603c09254fb5348cbcd96
JaiGobindPalSingh <jai.gobind.pal@gmail.com>
2025-01-26 15:07:10 +0530

    created the basic skeleton of the chat app in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-
+import Navbar from './components/Navbar.jsx'
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 const App = () => {
   return (
     <div>
-      <div className="flex bg-red-500 text-5xl">
-        hello
-      </div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/settings" element={<SettingsPage/>}/>
+          <Route path="/profile" element={<ProfilePage/>}/>
+        </Routes>
     </div>
   )
 }
