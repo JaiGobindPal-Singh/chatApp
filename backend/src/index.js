27cf8b4fdfa3d01cf1b1b7e2e7f95b22ebb41d85
JaiGobindPalSingh <jai.gobind.pal@gmail.com>
2025-01-29 19:23:38 +0530

    created the profile page solved problem of the image uploading

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use('/api/auth', authRoutes);
 app.use('/api/message',messageRoutes);
 
